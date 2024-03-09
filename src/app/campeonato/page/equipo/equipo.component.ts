b4836b814786c2ed20404d4d1b12071b17b78373
jesusvaldes2808 <jesus.valdes@cetus.com.co>
2024-03-09 12:29:11 -0500

    Buen funcionamiento

---
+++ src/app/campeonato/page/equipo/equipo.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'page-equipo',
+  templateUrl: './equipo.component.html',
+  styleUrls: ['./equipo.component.css']
+})
+export class EquipoComponent {
+
+}
